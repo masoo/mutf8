d91e24cd5df4c627ff0485e7637d0d40e0ce1a86
FUNABARA Masao <masao@masoo.jp>
2018-10-13 02:36:40 +0900

    add m_utf8_jump_table

--- mutf8.c
+++ mutf8.c
@@ -1,6 +1,30 @@
+#include <stdio.h>
+#include <stdint.h>
 #include "mutf8.h"
 
-int sample(int n)
-{
-    return n * 2;
-}
+/**
+ * @private
+ * @var uint8_t m_utf8_jump_table[256]
+ * @author Atsushi Enomoto  <atsushi@ximian.com>
+ * @sa https://github.com/corngood/mono/blob/master/eglib/src/gutf8.c
+ * 
+ * @note
+ *  base var is "const guchar g_utf8_jump_table[256]".
+ */
+/*
+ * Index into the table below with the first byte of a UTF-8 sequence to get
+ * the number of bytes that are supposed to follow it to complete the sequence.
+ *
+ * Note that *legal* UTF-8 values can't have 4 or 5-bytes. The table is left
+ * as-is for anyone who may want to do such conversion, which was allowed in
+ * earlier algorithms.
+*/
+const uint8_t m_utf8_jump_table[256] = {
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+    3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 1, 1};
--- test_main.c
+++ test_main.c
@@ -1,9 +1,26 @@
+#include <stdint.h>
 #include <assert.h>
+#include <locale.h>
 #include "mutf8.h"
+
+extern const uint8_t m_utf8_jump_table[];
 
 int main(void)
 {
-    int test = sample(1);
-    assert(test == 2);
+    setlocale(LC_ALL, "");
+
+    // test m_utf8_jump_table
+    char *test_str1 = u8"a";
+    int test_str1_size = m_utf8_jump_table[(uint8_t)(*test_str1)];
+    assert(test_str1_size == 1);
+    char *test_str2 = u8"あ";
+    int test_str2_size = m_utf8_jump_table[(uint8_t)(*test_str2)];
+    assert(test_str2_size == 3);
+    char *test_str3 = u8"🚀";
+    int test_str3_size = m_utf8_jump_table[(uint8_t)(*test_str3)];
+    assert(test_str3_size == 4);
+
+    // Don't insert value.
+    // m_utf8_jump_table[0] = 3;
     return 0;
 }
