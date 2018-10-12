238a700129b57f2e07721b534a359b9715e4abd0
FUNABARA Masao <masao@masoo.jp>
2018-10-13 02:57:39 +0900

    add m_utf8_ch_byte_size.

--- mutf8.c
+++ mutf8.c
@@ -15,11 +15,11 @@
  * Index into the table below with the first byte of a UTF-8 sequence to get
  * the number of bytes that are supposed to follow it to complete the sequence.
  *
- * Note that *legal* UTF-8 values can't have 4 or 5-bytes. The table is left
+ * Note that *legal* UTF-8 values can't have 5-bytes or 6-bytes. The table is left
  * as-is for anyone who may want to do such conversion, which was allowed in
  * earlier algorithms.
 */
-const uint8_t m_utf8_jump_table[256] = {
+static const uint8_t m_utf8_jump_table[256] = {
     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
@@ -28,3 +28,16 @@ const uint8_t m_utf8_jump_table[256] = {
     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
     2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
     3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 1, 1};
+
+/**
+ * @public
+ * @fn int64_t m_utf8_ch_byte_size(m_char8_t *str)
+ * @brief utf8 character byte size
+ * @param[in] utf8 character string
+ * @return utf8 character byte size
+ * @note that *legal* UTF-8 values can't have 5-bytes or 6-bytes.
+ */
+int64_t m_utf8_ch_byte_size(m_char8_t *str)
+{
+    return m_utf8_jump_table[(uint8_t)(*str)];
+}
--- mutf8.h
+++ mutf8.h
@@ -1,6 +1,7 @@
 #ifndef MUTF8_H
 #define MUTF8_H
 
-extern int sample(int n);
+typedef char m_char8_t;
+extern int64_t m_utf8_ch_byte_size(m_char8_t *str);
 
 #endif /* end MUTF_8 */
